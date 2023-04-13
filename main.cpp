8b03cfad6f316de67dbfdc32bc078032014922fc
Alex Bogushenko <bogushenkoas@ukr.net>
2023-04-13 18:02:47 +0300

    3 commit

--- main.cpp
+++ main.cpp
@@ -5,19 +5,61 @@ using namespace std;
 
 int main() {
 
-  int tenant [10][5] = { {1, 4, 3 , 4 , 5},
-                         {61, 7, 88 , 9 , 10},
-                         {11, 10, 13 , 14 , 15},
-                         {16, 7, 81 , 19 , 20},
-                         {3, 22, 5 , 24 , 25},
-                         {26, 27, 9 , 29 , 30},
-                         {31, 32, 12 , 34 , 35},
-                         {6, 37, 14 , 39 , 40},
-                         {41, 42, 43 , 7 , 45},
-                         {46, 3, 48 , 49 , 22}
-                                                                };
+  int tenant [10][10];
 
-int a = tenant[2][8];
-cout << a;
+  for(int i = 0; i < 10; i ++)
+  {
+      for(int j = 0; j < 10; j ++)
+      {
+           tenant[i][j] = 2;
+      }
+  }
+
+  for(int i = 0; i < 10; i ++)
+  {
+      tenant[i][i] = 0;
+  }
+    for(int i = 0; i < 10; i ++)
+    {
+        tenant[i][9 - i] = 0;
+    }
+
+
+  for(int i = 0; i < 10; i ++)
+  {
+      tenant[i][0] = 1;
+
+      for(int i = 0; i < 10; i ++)
+      {
+          tenant[i][9] = 1;
+
+          for(int j = 0; j < 10; j ++)
+          {
+              tenant[0][j] = 1;
+
+              for(int j = 0; j < 10; j ++)
+              {
+                  tenant[9][j] = 1;
+
+              }
+          }
+      }
+  }
+
+
+
+
+
+
+
+    for(int i = 0; i < 10; i ++)
+    {
+        for(int j = 0; j < 10; j ++)
+        {
+            cout << tenant[i][j] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
